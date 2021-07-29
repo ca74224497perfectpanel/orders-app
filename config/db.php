05ff1d7875420873d1f438342644bd44163d995e
Mikhail Aytbaev <ca74224497@perfectpanel.com>
2021-07-29 12:00:53 +0300

    Добавление моделей модуля, рефакторинг.

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=application',
-    'username' => 'root',
-    'password' => 'rootpwd',
+    'username' => 'ca74224497',
+    'password' => 'Tzj#VwacF7yE5pq',
     'charset' => 'utf8',
 ];
---
+++ modules/orders/models/Users.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\modules\orders\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['first_name', 'last_name'], 'required'],
+            [['first_name', 'last_name'], 'string', 'max' => 300],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return UsersQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new UsersQuery(get_called_class());
+    }
+}
